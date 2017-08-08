87425f0899de97f1e9f693fb26048fe44b4a18fb
liuweiwalliam <liuweiwalliam@163.com>
2017-08-08 20:03:27 +0800

    upd

--- home.js
+++ home.js
@@ -2,7 +2,7 @@
  * Created by walliam on 2017/2/7.
  */
 var content = [
-    ['原型目录', 'http://http://192.168.1.59:81/yilianAPP/%E4%B8%AA%E4%BA%BA%E4%B8%AD%E5%BF%83/#g=1&p='],
+    ['原型目录', 'http://192.168.1.188:8080/yilianAPP'],
     ['react首页', 'https://facebook.github.io/react/'],
     ['local-YL-微商城url', 'http://self.yilian.com/wechat/index.php'],
     ['local-YL-微商城ip', 'http://192.168.1.54/wechat/index.php'],
